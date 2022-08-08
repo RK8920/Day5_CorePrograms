8e2d9a53de502d6f35ff65ef8243633e7e85873d
Rohit Kamble <kamble.rohit10792@gmail.com>
2022-08-08 08:35:53 +0530

    Added: Functionality to check entered Number is Even or Odd

---
+++ EvenOrOdd.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.evenorodd;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Entered Number is EVEN");
+        } else
+            System.out.println("Entered Number is ODD");
+    }
+}
