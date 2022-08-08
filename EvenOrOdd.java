964d99e59d61b7a1f7f7f5fe48e84d7a16221315
Rohit Kamble <kamble.rohit10792@gmail.com>
2022-08-08 08:53:48 +0530

    Welcome to Even and Odd Program

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -4,12 +4,6 @@ import java.util.Scanner;
 
 public class EvenOrOdd {
     public static void main(String[] args) {
-        System.out.println("Enter a number");
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        if (num % 2 == 0) {
-            System.out.println("Entered Number is EVEN");
-        } else
-            System.out.println("Entered Number is ODD");
+        System.out.println("Welcome to Even and Odd Programs");
     }
 }
